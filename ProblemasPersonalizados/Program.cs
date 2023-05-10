af13372e16671fe1e98f332866c02a60423379e8
Victor Vale <joaovictorvale06@gmail.com>
2023-05-10 11:45:19 -0300

    "Seção 11"

---
+++ ProblemasPersonalizados/Program.cs
@@ -0,0 +1,50 @@
+namespace ProblemasPersonalizados;
+using ProblemasPersonalizados.Entities;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Clear();
+        Console.Write("Room number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.Write("Check-in date (dd/MM/yyyy): ");
+        DateTime checkIn = DateTime.Parse(Console.ReadLine());
+        Console.Write("Check-out date (dd/MM/yyyy): ");
+        DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+        if (checkOut <= checkIn)
+        {
+            Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+        }
+        else
+        {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+            
+            Console.WriteLine();
+            Console.WriteLine("Enter data to update the reservation: ");
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            checkOut = DateTime.Parse(Console.ReadLine());
+            
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                Console.WriteLine("Error in reservation: Reversation dates for update must be future dates");
+            }
+            else if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            }
+            else
+            {
+                reservation.UpdateDates(checkIn,checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+        }
+
+    }
+}

d57887fdd75af2b2fab8d1e443df5441660cfa49
Victor Vale <joaovictorvale06@gmail.com>
2023-05-25 10:14:31 -0300

    Finalizando a seção 13 - Trabalhando com arquivos

---
+++ Directory/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Directory
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/home/victorvale20/myfolder";
+
+            try
+            {
+                var folders = System.IO.Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                
+                var files = System.IO.Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                System.IO.Directory.CreateDirectory(path + "/newfolder");
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Path/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/home/victorvale20/myfolder/file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + System.IO.Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + System.IO.Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + System.IO.Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + System.IO.Path.GetFileName(path)); 
+            Console.WriteLine("GetExtension: " + System.IO.Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + System.IO.Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPath: " + System.IO.Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + System.IO.Path.GetTempPath());
+            
+        }
+    }
+}

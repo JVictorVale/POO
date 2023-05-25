991aaefb4e93aceb6f79bcddeef17e03a3909020
Victor Vale <joaovictorvale06@gmail.com>
2023-05-25 09:31:31 -0300

    ...

---
+++ ProblemasPersonalizados/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,15 @@
+using System;
+using ProblemasPersonalizados.Entities.Exceptions;
+
+namespace ProblemasPersonalizados.Entities.Exceptions;
+
+public class DomainException : ApplicationException
+{
+    public DomainException(string message) : base(message)
+    {
+        
+    }
+    
+    
+    
+}

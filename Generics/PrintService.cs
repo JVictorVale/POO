547312688330ca95683e21df24314ed83ab1315a
Victor Vale <joaovictorvale06@gmail.com>
2023-06-02 11:56:24 -0300

    Adicionando exercicio resolvido sobre conjuntos

--- Generics/PrintService.cs
+++ Generics/PrintService.cs
@@ -1,45 +1,34 @@
-namespace Generics
-{
+using System;
 
-    public class PrintService<T>
-    {
+namespace Generics {
+    class PrintService<T> {
+
         private T[] _values = new T[10];
         private int _count = 0;
 
-        public void AddValue(T value)
-        {
-            if (_count == 10)
-            {
+        public void AddValue(T value) {
+            if (_count == 10) {
                 throw new InvalidOperationException("PrintService is full");
             }
-
             _values[_count] = value;
             _count++;
         }
 
-        public T First()
-        {
-            if (_count == 0)
-            {
+        public T First() {
+            if (_count == 0) {
                 throw new InvalidOperationException("PrintService is empty");
             }
-
             return _values[0];
         }
 
-        public void Print()
-        {
+        public void Print() {
             Console.Write("[");
-            for (int i = 0; i < _count - 1; i++)
-            {
+            for (int i = 0; i < _count - 1; i++) {
                 Console.Write(_values[i] + ", ");
             }
-
-            if (_count > 0)
-            {
+            if (_count > 0) {
                 Console.Write(_values[_count - 1]);
             }
-
             Console.WriteLine("]");
         }
     }
